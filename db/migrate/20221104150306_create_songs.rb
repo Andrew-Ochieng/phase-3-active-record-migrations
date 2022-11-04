80a9aa33902ea9752bd129d414ddfa4899bbd33d
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-04 18:27:51 +0300

    Completed assignment

---
+++ db/migrate/20221104150306_create_songs.rb
@@ -0,0 +1,10 @@
+class CreateSongs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.string :artist
+      t.string :album
+      t.string :link
+    end
+  end
+end
